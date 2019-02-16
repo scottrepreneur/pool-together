81ee4a9b3c037fa0b3e1b3971a6004c274dff323
Dave Anderson <davepander@gmail.com>
2019-02-16 11:41:04 -0700

    Update PoolTogether.sol

--- contracts/PoolTogether.sol
+++ contracts/PoolTogether.sol
@@ -1,22 +1,26 @@
 pragma solidity ^0.5.0;
 
-import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
-import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
-
-
+//import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract PoolTogether {
+
+  address public owner = msg.sender;
+  uint creationTime = now;
+
   //States of Contract:
   //PoolOpen: Accepting
   //Saving: and Earning
   //PayOut: Withdraw
-  enum State {
-    PoolOpen,
-    Saving,
-    PayOut
+  enum States {
+    PoolOpen,  //0
+    Saving,    //1
+    PayOut     //2
   }
 
-  uint min = 0.5;
+  States state = States.PoolOpen;
+  uint pool = 0;
+  uint min = 1;
+
 
   modifier atState(States _state) {
     require(
@@ -28,7 +32,7 @@ contract PoolTogether {
 
   // Balances allowed to withdraw from contract
   mapping (address => uint) savings;
-}
+
 
 
 // Event emitted when a saver dives into the pool
@@ -38,33 +42,53 @@ contract PoolTogether {
   event Withdraw(address indexed saver, uint savings);
 
 //When a saver joins the pool
-function splash(uint deposit) public payable atState(State.PoolOpen) {
+function splash() public payable atState(States.PoolOpen) {
   //For use w/ DAI Tokens: require(transferFrom(msg.sender, address(this), deposit), "DRAW_FAILED");
-  require(msg.amount > min);
+  require(msg.value >= min);
+  pool = pool + msg.value;
+  savings[msg.sender] = msg.value;
 
 }
 
 
 //View Status of contract State
-function stateOfPool() public view returns (State) {
-    return State;
+function stateOfPool() public view returns(States){
+    return state;
   }
 
 //View Status of pool
+function poolSize() public view returns (uint){
+    return pool;
+}
 
-//View Status of pool potential return
+//View Status of pool potential return in 100x percentage value
+//So 300 = 3.00%
+//Hardcoded now
+function poolReturn() public view returns(uint){
+    return(300);
+}
 
 //View Status of saver deposit
-
-
+function myDeposit() public view returns(uint){
+    return(savings[msg.sender]);
+}
 
 
 //Withdraw, only during PayOut period
 //Question: Do we pull the funds from earning source each iteration?
 // Or do we only pull funds that are requested (maximizing roll-over)?
-function withdraw() public atState(State.PayOut) {
+function withdraw() public atState(States.PayOut) {
         uint amount = savings[msg.sender];
+        pool = pool - amount;
         // !re-entrancy : Zero the pending refund before
         savings[msg.sender] = 0;
         msg.sender.transfer(amount);
     }
+
+// Only for testing
+function setState(uint _stateVar) public {
+    require(msg.sender == owner);
+    state = States(_stateVar);
+}
+
+}
