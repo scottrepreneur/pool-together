4638f1a567e70a5b6c913cba95fc6919729a82c5
Dave Anderson <davepander@gmail.com>
2019-02-16 21:19:45 -0700

    9:15 Push

--- contracts/PoolTogether.sol
+++ contracts/PoolTogether.sol
@@ -1,11 +1,27 @@
 pragma solidity ^0.5.0;
-
+//import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 //import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract PoolTogether {
 
-  address public owner = msg.sender;
-  uint public creationTime = now;
+  address public owner;
+  uint public creationTime;
+  address[] public entrants;
+  //address[] private entrants;
+
+  // Balances allowed to withdraw from contract
+  mapping (address => uint) savings;
+  mapping (address => uint) entryMap;
+
+  uint pool = 0;
+  uint min = 10 finney;
+
+  constructor () public {
+    //address compoundFinanceMM = "0x3fda67f7583380e67ef93072294a7fac882fd7e7"
+    //address public rinkDai = "0x4e17c87c52d0e9a0cad3fbc53b77d9514f003807";
+    owner = msg.sender;
+    creationTime = now;
+  }
 
   //States of Contract:
   //PoolOpen: Accepting
@@ -18,9 +34,6 @@ contract PoolTogether {
   }
 
   States state = States.PoolOpen;
-  uint pool = 0;
-  uint min = 1 ether;
-
 
   modifier atState(States _state) {
     require(
@@ -30,65 +43,98 @@ contract PoolTogether {
     _;
   }
 
-  // Balances allowed to withdraw from contract
-  mapping (address => uint) savings;
 
+  // Event emitted when a saver dives into the pool
+  event Splash(address indexed saver, uint deposit, uint total);
 
-
-// Event emitted when a saver dives into the pool
-  event Splash(address indexed saver, uint deposit);
-
-// Event emitted when a saver withdraws
+  // Event emitted when a saver withdraws
   event Withdraw(address indexed saver, uint savings);
 
-//When a saver joins the pool
-function splash() public payable atState(States.PoolOpen) {
-  //For use w/ DAI Tokens: require(transferFrom(msg.sender, address(this), deposit), "DRAW_FAILED");
-  require(msg.value >= min);
-  pool = pool + msg.value;
-  savings[msg.sender] = savings[msg.sender] + msg.value;
-
-}
-
-
-//View Status of contract State
-function stateOfPool() public view returns(States){
-    return state;
-  }
-
-//View Status of pool
-function poolSize() public view returns (uint){
-    return pool;
-}
-
-//View Status of pool potential return in 100x percentage value
-//So 300 = 3.00%
-//Hardcoded now
-function poolReturn() public view returns(uint){
-    return(300);
-}
-
-//View Status of saver deposit
-function myDeposit() public view returns(uint){
-    return(savings[msg.sender]);
-}
-
-
-//Withdraw, only during PayOut period
-//Question: Do we pull the funds from earning source each iteration?
-// Or do we only pull funds that are requested (maximizing roll-over)?
-function withdraw() public atState(States.PayOut) {
-        uint amount = savings[msg.sender];
-        pool = pool - amount;
-        // !re-entrancy : Zero the pending refund before
-        savings[msg.sender] = 0;
-        msg.sender.transfer(amount);
+    //When a saver joins the pool
+    //Saver can add to deposit during PoolOpen
+    function splash() public payable atState(States.PoolOpen) {
+        //For use w/ DAI Tokens: require(transferFrom(msg.sender, address(this), deposit), "DRAW_FAILED");
+        require(msg.value >= min);
+        addEntrant(msg.sender);
+        pool = pool + msg.value;
+        savings[msg.sender] = savings[msg.sender] + msg.value;
+        emit Splash(msg.sender, msg.value, savings[msg.sender]);
     }
 
-// Only for testing
-function setState(uint _stateVar) public {
-    require(msg.sender == owner);
-    state = States(_stateVar);
-}
+    //Add enntrant to pool of potential winners
+    function addEntrant (address entrant) private {
+        if(savings[entrant] == 0){
+          //Only add to potential winners if current balance 0
+          entrants.push(entrant);
+          entryMap[entrant] = entrants.length - 1;
+        }
+        //Else: Already an active key
+    }
+
+    function checkDups() private {
+      // Check for duplicate addresses in entrants?
+    }
+
+    //Withdraw, only during PayOut period
+    //Question: Do we pull the funds from earning source each iteration?
+    // Or do we only pull funds that are requested (maximizing roll-over)?
+    function withdraw() public atState(States.PayOut) {
+          uint amount = savings[msg.sender];
+          pool = pool - amount;
+          // !re-entrancy : Zero the pending refund before
+          savings[msg.sender] = 0;
+          msg.sender.transfer(amount);
+          removeSaver();
+      }
+
+
+
+  function removeSaver() private {
+      //Copy last entry to slot occupied by withdrawing address
+      entrants[entryMap[msg.sender]] = entrants[entrants.length - 1];
+      //reset the entryMap  This is kind of risky with a 0 entrant index.
+      entryMap[msg.sender] = 0;
+      //delete the last entry element
+      if(entrants.length > 1){
+        entrants.pop();
+      } else {
+        //last entrant, set to owner
+        entrants[0] = owner;
+        //give balance to owner
+      }
+  }
+
+  //View Status of contract State
+  function stateOfPool() public view returns(States){
+      return state;
+  }
+
+  //View Status of pool
+  function poolSize() public view returns (uint){
+      return pool;
+  }
+
+  //View Status of pool potential return in 100x percentage value
+  //So 300 = 3.00%
+  //Hardcoded now
+  function poolReturn() public view returns(uint){
+      return(300);
+  }
+
+  //View Status of saver deposit
+  function myDeposit() public view returns(uint){
+      return(savings[msg.sender]);
+  }
+
+  //View Entry Status
+  function entryData() public view returns(uint Entries, uint Size){
+    return(entrants.length,pool);
+  }
+
+  // Only for testing
+  function setState(uint _stateVar) public {
+      require(msg.sender == owner);
+      state = States(_stateVar);
+  }
 
 }
